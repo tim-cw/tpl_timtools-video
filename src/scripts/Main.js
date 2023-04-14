14f9042cd730e07ccc751f113e6f4d42e8a3d8de
Matthieu Parent <matthieuparent@outlook.com>
2023-04-13 20:57:01 -0400

    working files

---
+++ src/scripts/Main.js
@@ -0,0 +1,17 @@
+// import ComponentFactory from './ComponentFactory';
+import Icons from './utils/Icons';
+
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    document.documentElement.classList.add('has-js');
+
+    Icons.load();
+
+    // new ComponentFactory();
+  }
+}
+new Main();
